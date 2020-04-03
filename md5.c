0ffa38292199a2108c2a1e8bb6a8fb277ef0ef54
Darren Regan <darren111894@instance-1.europe-west2-c.c.sha2-272919.internal>
2020-04-03 21:46:31 +0000

    Nexthash mostly done

--- md5.c
+++ md5.c
@@ -23,8 +23,8 @@
 #define S42 10
 #define S43 15
 #define S44 21
-/* ROTATE_LEFT rotates x left n bits.*/
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+/* ROTATE_LEFT rotates x left c bits.*/
+#define ROTATE_LEFT(x, c) (((x) << (c)) | ((x) >> (32-(c))))
 
 const uint32_t T[64] = {
         0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
@@ -67,6 +67,37 @@ uint32_t I(uint32_t x, uint32_t y, uint32_t z){
 	return y ^ (x | (~z));
 }
 
+//Page 10&11 of md5 memo
+void FF(uint32_t *a, uint32_t b, uint32_t c, uint32_t d, uint32_t k, uint32_t s, uint32_t ac) {
+    u_int32_t sum = (*a + F(b, c, d) + k + ac);
+    uint32_t f = F(b, c, d);
+    // printf("rotateLeft(%x + %x + %x + %x, %d)\n", *a, f, ac, k, s);
+    *a = b + LEFTROTATE(sum, s);
+}
+
+void GG(uint32_t *a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
+    u_int32_t sum = (*a + G(b, c, d) + x + ac);//(a) += F ((b), (c), (d)) + (x) + (UINT4)(ac);
+    uint32_t g = G(b, c, d);
+    //printf("rotateLeft(%x + %x + %x + %x, %d)\n", *a, g, ac, x, s);
+    *a = b + LEFTROTATE(sum, s);//(a) = ROTATE_LEFT ((a), (s));
+}
+
+void HH(uint32_t *a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
+    u_int32_t sum = (*a + H(b, c, d) + x + ac);
+    uint32_t h = H(b, c, d);
+    // printf("rotateLeft(%x + %x + %x + %x, %d)\n", *a, h, ac, x, s);
+    *a = b + LEFTROTATE(sum, s);
+}
+
+void II(uint32_t *a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac) {
+    u_int32_t sum = (*a + I(b, c, d) + x + ac);
+    uint32_t i = I(b, c, d);
+    // printf("rotateLeft(%x + %x + %x + %x, %d)\n", *a, i, ac, x, s);
+    *a = b + LEFTROTATE(sum, s);
+}
+
+
+
 enum flag {
 	READ, PAD0, PAD1, FINISH
 };
@@ -106,9 +137,94 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status)
 }
 
 void nexthash(union block *M, uint32_t *H){
-}
+	//Pg 11,12,13 md5 memo
+	uint32_t copyH[4];
+	copyH[0] = H[0];
+	copyH[1] = H[1];
+	copyH[2] = H[2];
+	copyH[3] = H[3];
 
+	/* Round 1 */
+	FF(&H[0], H[1], H[2], H[3], M->threetwo[0], S11, 0xd76aa478);  /* 1 */
+    	FF(&H[3], H[0], H[1], H[2], M->threetwo[1], S12, 0xe8c7b756);  /* 2 */
+    	FF(&H[2], H[3], H[0], H[1], M->threetwo[2], S13, 0x242070db);  /* 3 */
+    	FF(&H[1], H[2], H[3], H[0], M->threetwo[3], S14, 0xc1bdceee);  /* 4 */
+    	FF(&H[0], H[1], H[2], H[3], M->threetwo[4], S11, 0xf57c0faf);  /* 5 */
+    	FF(&H[3], H[0], H[1], H[2], M->threetwo[5], S12, 0x4787c62a);  /* 6 */
+    	FF(&H[2], H[3], H[0], H[1], M->threetwo[6], S13, 0xa8304613);  /* 7 */
+   	FF(&H[1], H[2], H[3], H[0], M->threetwo[7], S14, 0xfd469501);  /* 8 */
+    	FF(&H[0], H[1], H[2], H[3], M->threetwo[8], S11, 0x698098d8);  /* 9 */
+    	FF(&H[3], H[0], H[1], H[2], M->threetwo[9], S12, 0x8b44f7af);  /* 10 */
+    	FF(&H[2], H[3], H[0], H[1], M->threetwo[10], S13, 0xffff5bb1); /* 11 */
+    	FF(&H[1], H[2], H[3], H[0], M->threetwo[11], S14, 0x895cd7be); /* 12 */
+    	FF(&H[0], H[1], H[2], H[3], M->threetwo[12], S11, 0x6b901122); /* 13 */
+    	FF(&H[3], H[0], H[1], H[2], M->threetwo[13], S12, 0xfd987193); /* 14 */
+    	FF(&H[2], H[3], H[0], H[1], M->threetwo[14], S13, 0xa679438e); /* 15 */
+    	FF(&H[1], H[2], H[3], H[0], M->threetwo[15], S14, 0x49b40821); /* 16 */
+   	printf("\n");
 
+	/* Round 2 */
+   	GG(&H[0], H[1], H[2], H[3], M->threetwo[1], S21, 0xf61e2562);  /* 17 */
+    	GG(&H[3], H[0], H[1], H[2], M->threetwo[6], S22, 0xc040b340);  /* 18 */
+   	GG(&H[2], H[3], H[0], H[1], M->threetwo[11], S23, 0x265e5a51); /* 19 */
+     	GG(&H[1], H[2], H[3], H[0], M->threetwo[0], S24, 0xe9b6c7aa);  /* 20 */
+   	GG(&H[0], H[1], H[2], H[3], M->threetwo[5], S21, 0xd62f105d);  /* 21 */
+    	GG(&H[3], H[0], H[1], H[2], M->threetwo[10], S22, 0x2441453);  /* 22 */
+    	GG(&H[2], H[3], H[0], H[1], M->threetwo[15], S23, 0xd8a1e681); /* 23 */
+    	GG(&H[1], H[2], H[3], H[0], M->threetwo[4], S24, 0xe7d3fbc8);  /* 24 */
+    	GG(&H[0], H[1], H[2], H[3], M->threetwo[9], S21, 0x21e1cde6);  /* 25 */
+   	GG(&H[3], H[0], H[1], H[2], M->threetwo[14], S22, 0xc33707d6); /* 26 */
+    	GG(&H[2], H[3], H[0], H[1], M->threetwo[3], S23, 0xf4d50d87);  /* 27 */
+    	GG(&H[1], H[2], H[3], H[0], M->threetwo[8], S24, 0x455a14ed);  /* 28 */
+    	GG(&H[0], H[1], H[2], H[3], M->threetwo[13], S21, 0xa9e3e905); /* 29 */
+    	GG(&H[3], H[0], H[1], H[2], M->threetwo[2], S22, 0xfcefa3f8); /* 30 */
+    	GG(&H[2], H[3], H[0], H[1], M->threetwo[7], S23, 0x676f02d9); /* 31 */
+    	GG(&H[1], H[2], H[3], H[0], M->threetwo[12], S24, 0x8d2a4c8a); /* 32 */
+    	printf("\n");
+
+    	/* Round 3 */
+   	HH(&H[0], H[1], H[2], H[3], M->threetwo[5], S31, 0xfffa3942); /* 33 */
+    	HH(&H[3], H[0], H[1], H[2], M->threetwo[8], S32, 0x8771f681); /* 34 */
+    	HH(&H[2], H[3], H[0], H[1], M->threetwo[11], S33, 0x6d9d6122); /* 35 */
+    	HH(&H[1], H[2], H[3], H[0], M->threetwo[14], S34, 0xfde5380c); /* 36 */
+    	HH(&H[0], H[1], H[2], H[3], M->threetwo[1], S31, 0xa4beea44); /* 37 */
+    	HH(&H[3], H[0], H[1], H[2], M->threetwo[4], S32, 0x4bdecfa9); /* 38 */
+    	HH(&H[2], H[3], H[0], H[1], M->threetwo[7], S33, 0xf6bb4b60); /* 39 */
+    	HH(&H[1], H[2], H[3], H[0], M->threetwo[10], S34, 0xbebfbc70); /* 40 */
+    	HH(&H[0], H[1], H[2], H[3], M->threetwo[13], S31, 0x289b7ec6); /* 41 */
+    	HH(&H[3], H[0], H[1], H[2], M->threetwo[0], S32, 0xeaa127fa); /* 42 */
+    	HH(&H[2], H[3], H[0], H[1], M->threetwo[3], S33, 0xd4ef3085); /* 43 */
+    	HH(&H[1], H[2], H[3], H[0], M->threetwo[6], S34, 0x4881d05); /* 44 */
+    	HH(&H[0], H[1], H[2], H[3], M->threetwo[9], S31, 0xd9d4d039); /* 45 */
+    	HH(&H[3], H[0], H[1], H[2], M->threetwo[12], S32, 0xe6db99e5); /* 46 */
+   	HH(&H[2], H[3], H[0], H[1], M->threetwo[15], S33, 0x1fa27cf8); /* 47 */
+    	HH(&H[1], H[2], H[3], H[0], M->threetwo[2], S34, 0xc4ac5665); /* 48 */
+    	printf("\n");
+
+    	/* Round 4 */
+    	II(&H[0], H[1], H[2], H[3], M->threetwo[0], S41, 0xf4292244); /* 49 */
+    	II(&H[3], H[0], H[1], H[2], M->threetwo[7], S42, 0x432aff97); /* 50 */
+    	II(&H[2], H[3], H[0], H[1], M->threetwo[14], S43, 0xab9423a7); /* 51 */
+    	II(&H[1], H[2], H[3], H[0], M->threetwo[5], S44, 0xfc93a039); /* 52 */
+    	II(&H[0], H[1], H[2], H[3], M->threetwo[12], S41, 0x655b59c3); /* 53 */
+   	II(&H[3], H[0], H[1], H[2], M->threetwo[3], S42, 0x8f0ccc92); /* 54 */
+    	II(&H[2], H[3], H[0], H[1], M->threetwo[10], S43, 0xffeff47d); /* 55 */
+    	II(&H[1], H[2], H[3], H[0], M->threetwo[1], S44, 0x85845dd1); /* 56 */
+    	II(&H[0], H[1], H[2], H[3], M->threetwo[8], S41, 0x6fa87e4f); /* 57 */
+    	II(&H[3], H[0], H[1], H[2], M->threetwo[15], S42, 0xfe2ce6e0); /* 58 */
+    	II(&H[2], H[3], H[0], H[1], M->threetwo[6], S43, 0xa3014314); /* 59 */
+    	II(&H[1], H[2], H[3], H[0], M->threetwo[13], S44, 0x4e0811a1); /* 60 */
+    	II(&H[0], H[1], H[2], H[3], M->threetwo[4], S41, 0xf7537e82); /* 61 */
+    	II(&H[3], H[0], H[1], H[2], M->threetwo[11], S42, 0xbd3af235); /* 62 */
+    	II(&H[2], H[3], H[0], H[1], M->threetwo[2], S43, 0x2ad7d2bb); /* 63 */
+    	II(&H[1], H[2], H[3], H[0], M->threetwo[9], S44, 0xeb86d391); /* 64 */
+    	printf("\n");
+
+    	H[0] += copyH[0];
+    	H[1] += copyH[1];
+    	H[2] += copyH[2];
+   	H[3] += copyH[3];
+}//nexhhash
 
 int main(int argc, char *argv[]){
 

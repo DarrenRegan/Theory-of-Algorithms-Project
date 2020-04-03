a2696fe53a276c72fca0cccc95c13350944ecec0
Darren Regan <darren111894@instance-1.europe-west2-c.c.sha2-272919.internal>
2020-04-03 18:58:08 +0000

    Added nozerobytes to padding section

--- md5.c
+++ md5.c
@@ -2,16 +2,22 @@
 #include <inttypes.h>
 
 uint64_t nozerobytes(uint64_t nobits){
+
 	uint64_t result = 512ULL - (nobits % 512ULL);
 
 	if(result < 65)
 		result += 512;
 	
 	result -= 72;
+
+	// Tell num of bytes, of zeros needed to output inbetween the one bit and the 64 bit integer that prints at end
 	return (result / 8ULL);
 }
 
 int main(int argc, char *argv[]){
+
+	
+
 	if(argc != 2){
 		printf("Error: expected single filename as arugment. \n");
 		return 1;
@@ -24,11 +30,21 @@ int main(int argc, char *argv[]){
 	}
 
 	uint8_t b;
+	uint64_t nobits;
+
+	
 	//Reads 1 bit, 1 copy of 1 bit from infile
-	for(;fread(&b, 1, 1, infile) == 1;){
-		printf("%02x ", b);
+	for(nobits = 0; fread(&b, 1, 1, infile) == 1; nobits +=8){
+		printf("%02x ", PRIx8, b);
 	}
-	printf("\n");
+
+	printf("%02 ", PRIx8, 0x80); // Bits: 1000 0000
+	
+	for(uint64_t i = nozerobytes(nobits); i > 0; i--){
+		printf("%02x ", PRIx8, 0x00);
+	}
+
+	printf("%016 ", PRIx64, nobits);
 	fclose(infile);
 	
 	return 0;
